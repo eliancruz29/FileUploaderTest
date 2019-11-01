81488c82f42f1dbcedcedc711aa06c258e503c30
eliancruz29 <eliancruz29@gmail.com>
2019-10-31 20:48:23 -0400

    Adding some unit testing to the angular app

--- file-loader-web/ClientApp/src/app/components/uploader-file/uploader-file.component.spec.ts
+++ file-loader-web/ClientApp/src/app/components/uploader-file/uploader-file.component.spec.ts
@@ -1,18 +1,20 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, inject, tick } from '@angular/core/testing';
 
 import { UploaderFileComponent } from './uploader-file.component';
 import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ValidatorService } from 'src/app/services/validator/validator.service';
-import { ValidatorMockService } from 'src/app/test-mocks/services/validator/validator-mock.service';
 import { FileService } from 'src/app/services/file/file.service';
-import { FileMockService } from 'src/app/test-mocks/services/file/file-mock.service';
 import { AlertComponent } from 'src/app/widgets/alert/alert.component';
 import { LoadingComponent } from 'src/app/widgets/loading/loading.component';
 import { HttpClientModule } from '@angular/common/http';
+import { VALIDATION } from 'src/app/test-mocks/services/validator/mock-validator.service';
+import { of } from 'rxjs';
 
 describe('UploaderFileComponent', () => {
   let component: UploaderFileComponent;
   let fixture: ComponentFixture<UploaderFileComponent>;
+  let validatorService: ValidatorService;
+  let fileService: FileService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -27,9 +29,7 @@ describe('UploaderFileComponent', () => {
         ReactiveFormsModule
       ],
       providers: [
-        FormBuilder,
-        { provide: ValidatorService, useValue: ValidatorMockService },
-        { provide: FileService, useValue: FileMockService }
+        FormBuilder
       ]
     }).compileComponents();
   }));
@@ -37,7 +37,8 @@ describe('UploaderFileComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(UploaderFileComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    validatorService = fixture.debugElement.injector.get(ValidatorService);
+    fileService = fixture.debugElement.injector.get(FileService);
   });
 
   it('should create', () => {
@@ -50,5 +51,73 @@ describe('UploaderFileComponent', () => {
 
   it('should start with a input file Msg', () => {
     expect(component.fileMsg).toBe("Choose file");
+  });
+
+  it('form invalid when empty', () => {
+    spyOn(validatorService, 'getValidationData').and.returnValue(of(VALIDATION));
+
+    fixture.detectChanges();
+    expect(component.fileForm.valid).toBeFalsy();
+  });
+
+  it('form control name field validity', () => {
+    spyOn(validatorService, 'getValidationData').and.returnValue(of(VALIDATION));
+
+    fixture.detectChanges();
+    let name = component.fileForm.controls['name'];
+    expect(name.valid).toBeFalsy();
+
+    // name field is required
+    let errors = name.errors || {};
+    expect(errors['required']).toBeTruthy();
+
+    // Set name to something
+    name.setValue("Prueba");
+    errors = name.errors || {};
+    expect(errors['required']).toBeFalsy();
+  });
+
+  it('form control size field validity', () => {
+    spyOn(validatorService, 'getValidationData').and.returnValue(of(VALIDATION));
+
+    fixture.detectChanges();
+    let size = component.fileForm.controls['size'];
+    expect(size.valid).toBeTruthy();
+
+    // size field is required
+    let errors = size.errors || {};
+    expect(errors['max']).toBeFalsy();
+
+    // Set size to something
+    size.setValue(3097152);
+    errors = size.errors || {};
+    expect(errors['max']).toBeTruthy();
+
+    // Set size to something
+    size.setValue(1097152);
+    errors = size.errors || {};
+    expect(errors['max']).toBeFalsy();
+  });
+
+  it('form control type field validity', () => {
+    spyOn(validatorService, 'getValidationData').and.returnValue(of(VALIDATION));
+
+    fixture.detectChanges();
+    let type = component.fileForm.controls['type'];
+    expect(type.valid).toBeFalsy();
+
+    // size field is required
+    let errors = type.errors || {};
+    expect(errors['invalidType']).toBeTruthy();
+
+    // Set size to something
+    type.setValue('image/jpeg');
+    errors = type.errors || {};
+    expect(errors['invalidType']).toBeTruthy();
+
+    // Set size to something
+    type.setValue('application/pdf');
+    errors = type.errors || {};
+    expect(errors['invalidType']).toBeFalsy();
   });
 });
---
+++ file-loader-web/ClientApp/src/app/test-mocks/services/validator/mock-validator.service.ts
@@ -0,0 +1,6 @@
+import { IValidationData } from 'src/app/models/validation-data';
+
+export const VALIDATION: IValidationData = {
+  size: 2097152,
+  types: ['application/pdf', 'text/rtf', 'application/msword']
+} as IValidationData;
